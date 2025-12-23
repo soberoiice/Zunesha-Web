a91932e55aeb36f23f98c49879a37374c9d535dc
Royce Akhabhoa <royakhabhoa@gmail.com>
2025-12-23 17:03:20 +0100

    fix: api routing error

--- api/meta.js
+++ api/meta.js
@@ -11,6 +11,10 @@ export default async function handler(req, res) {
     const response = await axios.get(
       `https://api.myanimelist.net/v2/anime/${id}`,
       {
+        params: {
+          fields:
+            "id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics,videos,trailer",
+        },
         headers: {
           "X-MAL-CLIENT-ID": process.env.MAL_CLIENT_ID,
         },
