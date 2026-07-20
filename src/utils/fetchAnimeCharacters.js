21fb59e572627584b98399ea83947116b63f5ce8
soberoiice <akhabhoar@gmail.com>
2026-07-20 21:22:33 +0100

    fix streaming

--- src/utils/fetchAnimeCharacters.js
+++ src/utils/fetchAnimeCharacters.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+const PROXY_URL = import.meta.env.VITE_PROXY_URL;
+
 const API_BASE_URL =
   import.meta.env.VITE_DEPLOYMENT === "development"
     ? "http://localhost:3000/api/characters"
